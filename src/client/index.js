323771d6af8cbd5dd3050641f169b5080715d30f
Jeremy Teo <jeremyteo88@hotmail.com>
2021-01-10 12:28:16 +0800

    Convert CSS to SCSS styles.

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 import { handleSubmit } from './js/formHandler'
+import './styles/styles.scss'
 
 // Create an event listener for clicking the button
 document.getElementById('retrieve').addEventListener('click', handleSubmit);
